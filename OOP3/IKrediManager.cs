e0db655ce6d29f61f70352befc703df96b3b2fd9
okan çivgin <okanbjkspd@hotmail.com>
2021-01-23 23:53:42 +0300

    23.01.2020 Ders Sonu

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //interface şablon görevi görüyor.Birisi bu interface i kulanırsa kullanan kişi bu metodu kullanmak zorunda.Yani Kullanılması zorunluysa interface kullanırız
+    //interfaceleri birbirlerinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanırız.*********
+
+    interface IKrediManager
+    {
+        public void Hesapla();
+        void BiseyYap();
+        
+            
+        
+    }
+}
---
+++ Oop2/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+        //Eğerki bir nesnede bir değeri kullanmak zorunda gibi görünmüyorsan o nesneye ait değilmiş gibi duruyorsan o an , soyutlama hatası yaparsın
+
+
+    }
+}
